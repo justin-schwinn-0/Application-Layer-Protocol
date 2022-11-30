2ef4c832759e42b0fa79886405e76cb8669f462e
justin-schwinn-0 <justinsch1029@gmail.com>
2022-11-29 19:20:27 -0600

    pre rework  stuff

--- Server.py
+++ Server.py
@@ -1,9 +1,13 @@
 import socket
 import select
+import os
 
 HEADERSIZE = 10
 
 def main():
+
+    
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Binds the render to localhost port 1234
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((socket.gethostname(), 1249)) 
@@ -51,7 +55,7 @@ def main():
 
 
 
-def recieveMsg(sock):
+def recieveMsg(sock : socket.socket):
     try:
         fullMsg = ""
         msgLen = 0
@@ -69,16 +73,42 @@ def recieveMsg(sock):
     except:
         return False
 
-def sendMsg(sock, message):
+def sendMsg(sock :socket.socket, message : str):
     msg = f"{len(message):<{HEADERSIZE}}" + message
     sock.send(bytes(msg,"utf-8"))
 
-def getMediaList():
-    return "List"
+def getMediaList()-> str:
+    
+    fileList = os.listdir('files')
+
+    dataList = ""
+
+    #print(fileList)
+
+    for str in fileList:
+        dataList += str + "\n"
+    #print(dataList)
+
+    #print(f"sending list of {len(dataList)}...")
+
+    return dataList
     # Returns a string of all media in a file
 
-def getFile(filename):
-    return "Bytestream of file"
+def getFile(filename :str):
+
+    fileList = os.listdir('files')
+
+    if(filename in fileList):
+
+        print(f"File \'{filename}\' found...")
+        path = "files/"+filename
+        return open(path).read()
+    else:
+        return "File not found"
+                    
+
 
 if __name__ == "__main__":
-    main()
+    print(getMediaList())
+    print(getFile('stuff.txt'))
+    #main()
