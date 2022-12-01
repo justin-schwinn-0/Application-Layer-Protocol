b520f9435e083369e2cb9b48a56141259294c42b
justin-schwinn-0 <justinsch1029@gmail.com>
2022-12-01 17:55:02 -0600

    client fix

--- Client.py
+++ Client.py
@@ -24,11 +24,11 @@ def main():
 
     r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ##r.connect((renderIP, renderPort))
-    r.connect((socket.gethostname(), 31250)) # For testing on local host
+    r.connect((renderIP, 31250)) # For testing on local host
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ##s.connect((renderIP, renderPort))
-    s.connect((socket.gethostname(), 31249)) # For testing on local host
+    s.connect(serverIP, 31249) # For testing on local host
     sendMsg(s, "controller connected")
 
     userInput = ""
